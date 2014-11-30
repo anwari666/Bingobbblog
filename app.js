f695d001939fe74c4a04a0f20f4e7eb6ee4cf768
Anwari Ilman <anwari.ilman@gmail.com>
2014-11-30 15:07:52 +0100

    adding a functionality to reduce the guesses once they're called

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-function Tebakan(email, angka) {
+/* Class Guess
+contains email (string)
+and angka Guess (array of integers) */
+
+function Guess(email, angka) {
     this.email = email;
     this.angka = angka;
 
-    // returns true when this object contains a single number
+    // returns true when this object contains a number
     this.contains = function (num) {
         for (var i = 0; i < this.angka.length; i++) {
             if (this.angka[i] == num) {
@@ -11,37 +15,60 @@ function Tebakan(email, angka) {
         }
         return false;
     };
+
+    this.getAngka = function(){
+        return this.angka;
+    }
 }
 
-function Tebakans() {
-    this.tebakan = [];
+/*
+Class Guesses (Guesses)
+contains array of Guess / list of guesses
+and some methods to add new Guess and add new Guess into the list of guesses
+*/
+function Guesses() {
+    this.Guess = [];
     
-    this.addTebakan = function (tebakan) {
-        this.tebakan.push(tebakan);
+    this.addGuess = function (Guess) {
+        this.Guess.push(Guess);
     };
 
+    // checking the content of all Guesses against some numbers (array) in Result
     this.cekIsi = function (result) {
 
-        for (tebakan in this.tebakan) {
+        var currGuess = null;
+        var unGuessed = [];
 
-            var bingo = 0;
+        for (var idx in this.Guess) {
+
+            // set initial values
+            currGuess = this.Guess[idx];        // current Guess being investigated
+            unGuessed = currGuess.getAngka();   // remaining numbers
+
+            var correctGuess = 0;
+
+            // iterate over the result
             for (var i = 0; i < result.length; i++) {
-                if (tebakan.contains(result[i])) {
-                    bingo++;
+                if (currGuess.contains(result[i])) {
+                    correctGuess++;
+                
+                    unGuessed = unGuessed.filter(function(element){      // update some of the uncalled result...
+                        return element !== result[i];
+                    }); 
                 }
             }
 
-            if (bingo == 5) {
+            if (correctGuess == 5) {
                 console.log("BINGO!!");
             } else {
-                console.log(tebakan.email + " sudah cocok " + bingo + " angka. sisanya tinggal " + tebakan.angka.toString());
+                console.log(currGuess.email + " sudah cocok " + correctGuess + " angka. sisanya tinggal " + unGuessed.toString());
             }
         }
     };
 }
 
 function Result() {
-    this.angka = [];
+    this.angka = []; // array of called numbers
 
     this.addNew = function (num) {
         this.angka.push(num);
@@ -50,16 +77,16 @@ function Result() {
 
 }
 
-var guess_1 = new Tebakan("anwari.ilman@gmail.com", [3, 9, 11, 14, 17]);
-var guess_2 = new Tebakan("ujangsilet@gmail.com", [6, 9, 12, 15, 18]);
-var guess_3 = new Tebakan("wari@360looks.com",    [7, 14, 16, 18, 22]);
-var guess_4 = new Tebakan("waribisnis@gmail.com", [4, 6, 15, 17, 23]);
+var guess_1 = new Guess("anwari.ilman@gmail.com", [3, 9, 11, 14, 17]);
+var guess_2 = new Guess("ujangsilet@gmail.com", [6, 9, 12, 15, 18]);
+var guess_3 = new Guess("wari@360looks.com",    [7, 14, 16, 18, 22]);
+var guess_4 = new Guess("waribisnis@gmail.com", [4, 6, 15, 17, 23]);
 
-var guesses = new Tebakans();
-guesses.addTebakan(guess_1);
-guesses.addTebakan(guess_2);
-guesses.addTebakan(guess_3);
-guesses.addTebakan(guess_4);
+var guesses = new Guesses();
+guesses.addGuess(guess_1);
+guesses.addGuess(guess_2);
+guesses.addGuess(guess_3);
+guesses.addGuess(guess_4);
 
 var result = new Result();
 result.addNew(16);
